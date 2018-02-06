9f1bfef7a2eee987fd920afcda9f8c1db95ed9f3
SolovyovAlexander <36075751+SolovyovAlexander@users.noreply.github.com>
2018-02-06 19:55:45 +0300

    Add files via upload

--- app/src/main/java/com/example/niklss/innolib/Activites/InnoLib.java
+++ app/src/main/java/com/example/niklss/innolib/Activites/InnoLib.java
@@ -1,16 +1,19 @@
 package com.example.niklss.innolib.Activites;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.niklss.innolib.DataBase.DataBaseHelper;
+import com.example.niklss.innolib.DataBase.Base;
 import com.example.niklss.innolib.DataBase.DbRepository;
 import com.example.niklss.innolib.R;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -19,7 +22,7 @@ import java.util.ArrayList;
 
 public class InnoLib extends AppCompatActivity {
 
-    private DataBaseHelper mDBHelper;
+    private Base mDBHelper;
     private SQLiteDatabase mDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,26 +33,23 @@ public class InnoLib extends AppCompatActivity {
         catalog.setOnClickListener(clCatalog);
 
         //database initialization
-//        mDBHelper = new Base(this);
-//        try {
-//            mDBHelper.updateDataBase();
-//        } catch (IOException mIOException) {
-//            throw new Error("UnableToUpdateDatabase");
-//        }
-//
-//        try {
-//            mDb = mDBHelper.getWritableDatabase();
-//        } catch (SQLException mSQLException) {
-//            throw mSQLException;
-//        }
-//
-//        ArrayList<String[]> list = getBooks();
-//        for (int i = 0; i <list.size() ; i++) {
-//            for (int j = 0; j <list.get(i).length ; j++) {
-//                System.out.print(list.get(i)[j]+" ");
-//            }
-//            System.out.println("");
-//        }
+       mDBHelper = new Base(this);
+        try {
+            mDBHelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+
+        try {
+            mDb = mDBHelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
+
+
+
+
+
 
 
 
@@ -75,6 +75,8 @@ public class InnoLib extends AppCompatActivity {
         ArrayList<String[]> Data = repository.getDataArticles();
         return Data;
     }
+
+
 
 
 
--- app/src/main/java/com/example/niklss/innolib/Activites/Login.java
+++ app/src/main/java/com/example/niklss/innolib/Activites/Login.java
@@ -7,10 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.niklss.innolib.DataBase.DataBaseHelper;
 import com.example.niklss.innolib.R;
-
-import java.io.IOException;
 
 /**
  * Created by user on 04.02.2018.
@@ -24,20 +21,7 @@ public class Login extends Activity {
         Button bt=(Button) findViewById(R.id.button);
         EditText name=(EditText) findViewById(R.id.editText);
         EditText password=(EditText) findViewById(R.id.editText2);
-
         bt.setOnClickListener(click);
-
-        DataBaseHelper a = new DataBaseHelper(Login.this);
-        try {
-            a.createDataBase();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        a.addMember();
-        a.getString();
-
-
     }
 
     View.OnClickListener click=new View.OnClickListener() {
