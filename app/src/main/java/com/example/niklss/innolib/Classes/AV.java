13fb8e6a1557a5fd8a0f8509b1ace58d10338904
Albert Badretdinov <al.badretdinov@innopolis.ru>
2018-03-31 22:52:15 +0300

    More classes

---
+++ app/src/main/java/com/example/niklss/innolib/Classes/AV.java
@@ -0,0 +1,65 @@
+package com.example.niklss.innolib.Classes;
+
+/**
+ * Created by Niklss on 31.03.2018.
+ */
+
+public class AV {
+
+    private String title;
+    private String authors;
+    private int avId;
+    private int countAv;
+    private UserCard user;
+    private int daysLeft;
+
+    public void AV(String title, String authors, int avId, int countAv){
+        this.title = title;
+        this.authors = authors;
+        this.avId = avId;
+        this.countAv = countAv;
+    }
+
+    public void AV(String[] av){
+        this.title = av[0];
+        this.authors = av[1];
+        this.avId = Integer.parseInt(av[2]);
+        this.countAv = Integer.parseInt(av[3]);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public int getAvId() {
+        return avId;
+    }
+
+    public int getCountAv() {
+        return countAv;
+    }
+
+    public void setCountAv(int countAv) {
+        this.countAv = countAv;
+    }
+
+    public UserCard getUser() {
+        return user;
+    }
+
+    public void setUser(UserCard user) {
+        this.user = user;
+    }
+
+    public int getDaysLeft() {
+        return daysLeft;
+    }
+
+    public void setDaysLeft(int daysLeft) {
+        this.daysLeft = daysLeft;
+    }
+}
---
+++ app/src/main/java/com/example/niklss/innolib/Classes/Articles.java
@@ -0,0 +1,93 @@
+package com.example.niklss.innolib.Classes;
+
+/**
+ * Created by Niklss on 31.03.2018.
+ */
+
+public class Articles {
+
+    private String title;
+    private String authors;
+    private String jtitle;
+    private String issue;
+    private String date;
+    private String editor;
+    private int avId;
+    private int countAv;
+    private UserCard user;
+    private int daysLeft;
+
+    public void Articles(String title, String authors, String jtitle, String issue, String date, String editor, int avId, int countAv){
+        this.title = title;
+        this.authors = authors;
+        this.jtitle = jtitle;
+        this.issue = issue;
+        this.date = date;
+        this.editor = editor;
+        this.avId = avId;
+        this.countAv = countAv;
+    }
+
+    public void Articles(String[] av){
+        this.title = av[0];
+        this.authors = av[1];
+        this.jtitle = av[2];
+        this.issue = av[3];
+        this.date = av[4];
+        this.editor = av[5];
+        this.avId = Integer.parseInt(av[6]);
+        this.countAv = Integer.parseInt(av[7]);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public String getJtitle() {
+        return jtitle;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public int getAvId() {
+        return avId;
+    }
+
+    public int getCountAv() {
+        return countAv;
+    }
+
+    public void setCountAv(int countAv) {
+        this.countAv = countAv;
+    }
+
+    public UserCard getUser() {
+        return user;
+    }
+
+    public void setUser(UserCard user) {
+        this.user = user;
+    }
+
+    public int getDaysLeft() {
+        return daysLeft;
+    }
+
+    public void setDaysLeft(int daysLeft) {
+        this.daysLeft = daysLeft;
+    }
+}
