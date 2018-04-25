1aceb63057447945de1ff54447448b6bd888e025
DenisLevkovets <levkovets_da@mail.ru>
2018-04-25 18:48:53 +0300

    Merge remote-tracking branch 'origin/master' into denis
    # Conflicts:
    #	app/src/main/assets/Libary.db

--- app/src/main/java/com/example/niklss/innolib/Activites/Login.java
+++ app/src/main/java/com/example/niklss/innolib/Activites/Login.java
@@ -2,18 +2,23 @@ package com.example.niklss.innolib.Activites;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.niklss.innolib.Classes.Books;
+import com.example.niklss.innolib.Classes.Patron;
 import com.example.niklss.innolib.DataBase.DataBaseHelper;
 import com.example.niklss.innolib.R;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by user on 04.02.2018.
@@ -25,6 +30,7 @@ public class Login extends Activity {
     TextView tx;
     DataBaseHelper db=null;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
